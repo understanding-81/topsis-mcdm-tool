fa5384f26fa08021bbf07a859d0f4bcee3050060
Sartaj <sartajvirdi@gmail.com>
2026-01-21 11:15:12 +0530

    Create ResultTable.jsx

---
+++ frontend/src/components/ResultTable.jsx
@@ -0,0 +1,26 @@
+export default function ResultTable({ data }) {
+  if (!data || data.length === 0) return null;
+
+  return (
+    <div className="table-responsive mt-3">
+      <table className="table table-bordered table-striped">
+        <thead>
+          <tr>
+            {Object.keys(data[0]).map((key) => (
+              <th key={key}>{key}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, i) => (
+            <tr key={i}>
+              {Object.values(row).map((value, j) => (
+                <td key={j}>{value}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
