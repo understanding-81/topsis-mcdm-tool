319afc459a004bab04850a3266d746eb08b4aa6b
Sartaj <sartajvirdi@gmail.com>
2026-01-21 12:43:29 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://topsis-backend.onrender.com",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
 });
 
 export default api;
